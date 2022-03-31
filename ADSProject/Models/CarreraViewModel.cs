5ed718d9a78257d69cdea9611d8a1396ed24151a
Elias <eamm2233@.com>
2022-03-31 17:01:00 -0600

    Agregar archivos de proyecto.

---
+++ ADSProject/Models/CarreraViewModel.cs
@@ -0,0 +1,24 @@
+﻿using ADSProject.Utils;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ADSProject.Models
+{
+    public class CarreraViewModel
+    {
+        [Display(Name = "ID")]
+        public int idCarrera { get; set; }
+
+        [Required(ErrorMessage = Constants.REQUIRED_FIELD)]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "La longitud del campo no debe ser mayor a 50 caracteres ni menor de 3 caracteres.")]
+        [Display(Name = "Código")]
+        public string codigoCarrera { get; set; }
+        [Required(ErrorMessage = Constants.REQUIRED_FIELD)]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "La longitud del campo no debe ser mayor a 50 caracteres ni menor de 5 caracteres.")]
+        [Display(Name = "Carrera")]
+        public string nombreCarrera { get; set; }
+    }
+}
---
+++ ADSProject/Repository/IProfesorRepository.cs
@@ -0,0 +1,21 @@
+﻿using ADSProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ADSProject.Repository
+{
+    public interface IProfesorRepository
+    {
+        List<ProfesorViewModel> obtenerProfesor();
+
+        int agregarProfesor(ProfesorViewModel profesorViewModel);
+
+        int actualizarProfesor(int idProfeosr, ProfesorViewModel profesorViewModel);
+
+        bool eliminarProfesor(int idProfesor);
+
+        ProfesorViewModel obtenerProfesorPorID(int idProfesor);
+    }
+}
