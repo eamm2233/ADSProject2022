3ad3c119951a6c31ce03846ac7335bbc6021e69b
Elias <eamm2233@.com>
2022-04-21 22:25:37 -0600

    Creacion de la entidad Grupo

---
+++ ADSProject/Controllers/GrupoController.cs
@@ -0,0 +1,98 @@
+﻿using ADSProject.Models;
+using ADSProject.Repository;
+using ADSProject.Utils;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ADSProject.Controllers
+{
+    public class GrupoController : Controller
+    {
+        private readonly IGrupoRepository grupoRepository;
+
+        public GrupoController(IGrupoRepository grupoRepository)
+        {
+            this.grupoRepository = grupoRepository;
+        }
+        public IActionResult Index()
+        {
+            try
+            {
+                var item = grupoRepository.ObtenerGrupos();
+
+                return View(item);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        [HttpGet]
+        public IActionResult Form(int? idGrupo, Operaciones operaciones)
+        {
+            try
+            {
+                var grupo = new GrupoViewModel();
+
+                if (idGrupo.HasValue)
+                {
+                    grupo = grupoRepository.obtenerGrupoPorID(idGrupo.Value);
+                }
+                // Indica el tipo de operacion que es esta realizando
+                ViewData["Operaciones"] = operaciones;
+
+                return View(grupo);
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Form(GrupoViewModel grupoViewModel)
+        {
+            try
+            {
+                if (grupoViewModel.idGrupo == 0) // En caso de insertar
+                {
+                    grupoRepository.agregarGrupo(grupoViewModel);
+                }
+                else // En caso de actualizar
+                {
+                    grupoRepository.actualizarGrupo
+                        (grupoViewModel.idGrupo, grupoViewModel);
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int idGrupo)
+        {
+            try
+            {
+                grupoRepository.eliminarGrupo(idGrupo);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ADSProject/Models/GrupoViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ADSProject.Utils;
+using System.ComponentModel.DataAnnotations;
+
+namespace ADSProject.Models
+{
+    public class GrupoViewModel
+    {
+        [Display(Name = "ID")]
+        public int idGrupo { get; set; }
+
+        [Required(ErrorMessage = Constants.REQUIRED_FIELD)]
+        [Display(Name = "ID Carrera")]
+        public int idCarrera { get; set; }
+
+        [Required(ErrorMessage = Constants.REQUIRED_FIELD)]
+        [Display(Name = "ID Materia")]
+        public int idMateria { get; set; }
+
+        [Required(ErrorMessage = Constants.REQUIRED_FIELD)]
+        [Display(Name = "ID Profesor")]
+        public int idProfesor { get; set; }
+
+        [Required(ErrorMessage = Constants.REQUIRED_FIELD)]
+        [Display(Name = "Ciclo")]
+        public string ciclo { get; set; }
+
+        [Required(ErrorMessage = Constants.REQUIRED_FIELD)]
+        [Display(Name = "Año")]
+        public string anio { get; set; }
+    }
+}
